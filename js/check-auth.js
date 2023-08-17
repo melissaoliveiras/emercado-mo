fae172bc6222cecde99fde6f5657914a94fd2e80
choniih <n.heguaburo@gmail.com>
2023-08-17 18:38:15 -0300

    login actualizado

--- js/check-auth.js
+++ js/check-auth.js
@@ -2,10 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const isLoggedIn = localStorage.getItem("isLoggedIn") || sessionStorage.getItem("isLoggedIn");
     const isLoginPage = window.location.pathname.endsWith("login.html");
   
-    if (!isLoggedIn) {
-      if (!isLoginPage) {
+    if (!isLoggedIn){
         window.location.href = "login.html";
       }
-    }
+   
   });
   
